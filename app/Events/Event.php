d47a55c6a837771983feca01a97d62e08acb2335
A Wilde <annalisa.wilde@gmail.com>
2015-11-12 22:21:52 -0500

    Initial Deploy

---
+++ app/Events/Event.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace Howoldishill\Events;
+
+abstract class Event
+{
+    //
+}
---
+++ app/Providers/EventServiceProvider.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Howoldishill\Providers;
+
+use Illuminate\Contracts\Events\Dispatcher as DispatcherContract;
+use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
+
+class EventServiceProvider extends ServiceProvider
+{
+    /**
+     * The event listener mappings for the application.
+     *
+     * @var array
+     */
+    protected $listen = [
+        'Howoldishill\Events\SomeEvent' => [
+            'Howoldishill\Listeners\EventListener',
+        ],
+    ];
+
+    /**
+     * Register any other events for your application.
+     *
+     * @param  \Illuminate\Contracts\Events\Dispatcher  $events
+     * @return void
+     */
+    public function boot(DispatcherContract $events)
+    {
+        parent::boot($events);
+
+        //
+    }
+}
